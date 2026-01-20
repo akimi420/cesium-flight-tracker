de9e19f3ee7d0feea3ecb523e2bac8e3ffa9bd26
Akimi Yamamoto <p2320199@myu.ac.jp>
2026-01-20 17:05:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Cesium.Ion.defaultAccessToken = "ここにあなたのIonトークン";
+Cesium.Ion.defaultAccessToken =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmMGFlMzRjZi0xMTg2LTQyMWItYjEyOS02YWNlZTk4NDY2OTUiLCJpZCI6MzgxMDgzLCJpYXQiOjE3Njg4OTUxNzd9.thBsRrp8DmJxjSndUnz6rSJTf0VtEfqGSRE-OWEdznA;
 
 const viewer = new Cesium.Viewer("cesiumContainer", {
   terrain: Cesium.Terrain.fromWorldTerrain()
